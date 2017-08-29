b454f767498fb33263d283d6eb4309e89afe5695
Alumno <Alumno@LAB402-06>
2017-08-29 11:16:02 -0500

    quinto_message

---
+++ lab2ExampleGitHubSegundoR2A/src/main/java/lab03/c15/a/segundo/repository/lab2ExampleGitHubSegundoR2A/Prueba.java
@@ -0,0 +1,12 @@
+package lab03.c15.a.segundo.repository.lab2ExampleGitHubSegundoR2A;
+
+public class Prueba {
+public static void main(String[] args) {
+	System.out.println("Hola mundo aqui estoy de nuevo");
+}
+private static void hola() {
+	System.out.println("Hola amegos");
+}
+//---
+}
+ 

e18731a6475a58abc95b3131e38d25ebaf71f6e3
Trevor <trevdog16@gmail.com>
2017-08-29 09:09:31 -0500

    SegundoRepository

---
+++ lab2ExampleGitHubSegundoR2A/src/main/java/lab03/c15/a/segundo/repository/lab2ExampleGitHubSegundoR2A/App.java
@@ -0,0 +1,13 @@
+package lab03.c15.a.segundo.repository.lab2ExampleGitHubSegundoR2A;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

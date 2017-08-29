33baf8e8c29d721cca3e88cc866e00f17a92ce77
Alumno <Alumno@LAB402-06>
2017-08-29 09:28:39 -0500

    Segundo_Cambio_Wey!!

--- lab2ExampleGitHubSegundoR2A/src/main/java/lab03/c15/a/segundo/repository/lab2ExampleGitHubSegundoR2A/StudentController.java
+++ lab2ExampleGitHubSegundoR2A/src/main/java/lab03/c15/a/segundo/repository/lab2ExampleGitHubSegundoR2A/StudentController.java
@@ -4,7 +4,7 @@ package lab03.c15.a.segundo.repository.lab2ExampleGitHubSegundoR2A;
  * Hello world!
  *
  */
-public class App 
+public class StudentController 
 {
     public static void main( String[] args )
     {
